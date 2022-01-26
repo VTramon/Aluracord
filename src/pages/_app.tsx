4a6f7c3f8fcc87e2e860a3e94d5a967395df1e71
VTramon <vitorr.ooliveira@gmail.com>
2022-01-26 14:05:21 -0300

    improving the componentization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,35 +1,5 @@
-import type { AppProps /*, AppContext */ } from 'next/app'
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  )
-}
+import type { AppProps } from 'next/app'
+import { GlobalStyle } from '../GlobalStyle'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
